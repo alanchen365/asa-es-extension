3206af47983ebe8515d020f0f930504287e14ff3
alanchen365@gm <alanchen365@gmail.com>
2018-09-13 11:54:30 +0800

    修正验证器和分页

--- src/Base/BaseValidate.php
+++ src/Base/BaseValidate.php
@@ -53,8 +53,7 @@ class BaseValidate extends Validate
      */
     public function verify(string $scene, array $vData, int $code = 1000): void
     {
-        // 验证规则为空的话 默认不验证
-        if ($this->getScene($scene)) {
+        if(!empty($this->scene['$scene'])){
             if (!$this->scene($scene)->check($vData)) {
                 throw new AppException($code, $this->getError());
             }

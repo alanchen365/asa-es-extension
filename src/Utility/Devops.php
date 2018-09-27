e363274ad03756e94022e023f63f8b7230973f31
alanchen365@gm <alanchen365@gmail.com>
2018-09-27 09:14:12 +0800

    删除缓存

--- src/Utility/Devops.php
+++ src/Utility/Devops.php
@@ -26,4 +26,38 @@ class Devops
 
         return $keys;
     }
+
+
+    /**
+    * 清除某个表所有的redis缓存
+    */
+    public static function clearModuleAllRedisCache($redisObj, string $tableName)
+    {
+        $keys = Devops::getModuleAllRedisKeys($tableName);
+        foreach ($keys as $key) {
+            $redisObj->delAll($redisObj->keys($key."*"));
+        }
+        
+        return  Devops::getModuleAllRedisKeys($tableName);
+    }
+
+    /**
+     * 清除某个表部分的redis缓存
+     */
+    public static function clearModuleAllRedisCacheById($redisObj, string $tableName, array $ids)
+    {
+        $key =  EsRedis::getBeanKeyPre($tableName, AsaEsConst::REDIS_BASIC_TYPE);
+        $keys = $redisObj->keys($key."*");
+
+        $pipe = $redisObj->multi(\Redis::PIPELINE);
+        foreach ($ids as $id) {
+            $redisObj->del($key."_{$id}");
+        }
+
+        $redisObj->del(EsRedis::getBeanKeyPre($tableName, AsaEsConst::REDIS_BASIC_GET_ALL));
+        $redisObj->del(EsRedis::getBeanKeyPre($tableName, AsaEsConst::REDIS_BASIC_SEARCH_ALL));
+
+        $pipe->exec();
+        return  Devops::getModuleAllRedisKeys($tableName);
+    }
 }

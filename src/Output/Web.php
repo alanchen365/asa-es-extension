ef6b7f92ea21140876c238e99c09e98a3b0ca677
alanchen365@gm <alanchen365@gmail.com>
2018-10-30 11:06:36 +0800

    记录logh

--- src/Output/Web.php
+++ src/Output/Web.php
@@ -30,16 +30,18 @@ class Web
         }
 
         $requestObj = Di::getInstance()->get(AsaEsConst::DI_REQUEST_OBJ);
-//        $saveData = [
-//                AsaEsConst::REQUEST_ID => $requestObj->getRequestId(),
-//                'swoole_http_request' => (array)$requestObj->getSwooleRequest(),
+        $saveData = [
+                AsaEsConst::REQUEST_ID => $requestObj->getRequestId(),
+                'swoole_http_request' => (array)$requestObj->getSwooleRequest(),
+                'response_code' =>$code,
+                'response_msg' => $msg,
 //                'response_body' => $data
-//            ];
-//
-//        // 异步写文件
-//        TaskManager::async(function () use ($saveData) {
-//            FileLogger::getInstance()->log(json_encode($saveData), AsaEsConst::LOG_REQUEST_RESPONSE);
-//        });
+            ];
+
+        // 异步写文件
+        TaskManager::async(function () use ($saveData) {
+            FileLogger::getInstance()->log(json_encode($saveData), AsaEsConst::LOG_REQUEST_RESPONSE);
+        });
 
         $response->withAddedHeader('request_id',$requestObj->getRequestId());
         $response->write(json_encode($data));

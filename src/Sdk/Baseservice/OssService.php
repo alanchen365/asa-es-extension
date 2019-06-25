582a1be784595bec4058faf2122c54077cb62c2f
Bole <Achuner@AchunerdeMacBook-Pro.local>
2019-06-25 18:50:48 +0800

    加入保存临时文件

--- src/Sdk/Baseservice/OssService.php
+++ src/Sdk/Baseservice/OssService.php
@@ -31,26 +31,26 @@ class OssService extends BaseBaseservice {
      */
     public static function uploads(UploadFile $file, string $path , bool $isIgnoreErr = false) :array {
 
-        // 参数整理
-        $requestParams = [
-            'system_id' => AppInfo::SYSTEM_ID,
-        ];
-
-        // 获取配置文件
-        $res = null;
-        $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
-        $remoteService->setIsIgnoreErr($isIgnoreErr);
-        $remoteService->getInstance(RpcConst::TRACKING_RRC_SERVICE_CONF);
-        $res = $remoteService->request(RpcConst::TRACKING_RRC_SERVICE_CONF['serviceName'],'Oss','getOssConfig',$requestParams);
-
-        $ossConfig = $res['oss-config'] ?? null;
-        if (!$ossConfig) {
-            throw new MsgException(1000, 'Oss文件上传失败');
-        }
-
-        $ossClient = new OssClient($ossConfig['access_id'], $ossConfig['access_key'], $ossConfig['endpoint']);
-
         try {
+            // 参数整理
+            $requestParams = [
+                'system_id' => AppInfo::SYSTEM_ID,
+            ];
+
+            // 获取配置文件
+            $res = null;
+            $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
+            $remoteService->setIsIgnoreErr($isIgnoreErr);
+            $remoteService->getInstance(RpcConst::TRACKING_RRC_SERVICE_CONF);
+            $res = $remoteService->request(RpcConst::TRACKING_RRC_SERVICE_CONF['serviceName'],'Oss','getOssConfig',$requestParams);
+
+            $ossConfig = $res['oss-config'] ?? null;
+            if (!$ossConfig) {
+                throw new MsgException(1000, 'Oss文件上传失败');
+            }
+
+            $ossClient = new OssClient($ossConfig['access_id'], $ossConfig['access_key'], $ossConfig['endpoint']);
+
             // 截取上传原始文件信息
             $path = trim($path, '/');
             $original = $file->getClientFilename();
@@ -82,6 +82,62 @@ class OssService extends BaseBaseservice {
 
             return $res['oss-file'];
         } catch (\Throwable $throwable) {
+            throw new AppException($throwable->getCode(),$throwable->getMessage());
+        }
+    }
+
+
+    /**
+     * $original 文件目录 /server/php/Temp/1.txt
+     * 保存临时文件
+     */
+    public static function saveTmpFile(string $original, string $savePath = 'tmp', ?string $tmpFilename = '', bool $isIgnoreErr = false){
+
+        try{
+            // 参数整理
+            $requestParams = [
+                'system_id' => AppInfo::SYSTEM_ID,
+            ];
+        
+            $ext = pathinfo($original, PATHINFO_EXTENSION);
+            $tmpFilename = empty($tmpFilename) ? date('YmdHis').".{$ext}" : $tmpFilename.".{$ext}";
+
+            // 获取配置文件
+            $res = null;
+            $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
+            $remoteService->setIsIgnoreErr($isIgnoreErr);
+            $remoteService->getInstance(RpcConst::TRACKING_RRC_SERVICE_CONF);
+            $res = $remoteService->request(RpcConst::TRACKING_RRC_SERVICE_CONF['serviceName'],'Oss','getOssConfig',$requestParams);
+
+            $ossConfig = $res['oss-config'] ?? null;
+            if (!$ossConfig) {
+                throw new MsgException(1000, 'Oss文件上传失败');
+            }
+            $ossClient = new OssClient($ossConfig['access_id'], $ossConfig['access_key'], $ossConfig['endpoint']);
+
+            // 截取上传原始文件信息
+            $path = trim($savePath, '/');
+            $location = "{$savePath}/{$tmpFilename}";
+
+            $result = $ossClient->uploadFile($ossConfig['bucket'], $location, $original);
+            if (false === $result) {
+                throw new MsgException(1000, 'Oss文件上传失败');
+            }
+
+            // 保存上传日志
+            $requestParams = [
+                'system_id' => AppInfo::SYSTEM_ID,
+                'hash' => $result['x-oss-hash-crc64ecma'] ?? '',
+                'host' => $result['oss-requestheaders']['Host'] ?? '',
+                'original' => $tmpFilename,
+                'location' => "/".$location,
+                'ext' => $ext,
+            ];
+
+            $res = $remoteService->request(RpcConst::TRACKING_RRC_SERVICE_CONF['serviceName'],'Oss','insertUprcord',$requestParams);
+            return $res['oss-file'];
+
+        }catch (\Throwable $throwable) {
             throw new AppException($throwable->getCode(),$throwable->getMessage());
         }
     }
--- src/Utility/ExcelUtility.php
+++ src/Utility/ExcelUtility.php
@@ -4,11 +4,13 @@ namespace AsaEs\Utility;
 
 use AsaEs\AsaEsConst;
 use AsaEs\Config;
+use AsaEs\Sdk\Baseservice\OssService;
 use EasySwoole\Core\Component\Di;
 use EasySwoole\Core\Http\Message\UploadFile;
 use EasySwoole\Core\Swoole\ServerManager;
 use PHPExcel_IOFactory;
 use PHPExcel_Worksheet;
+use EasySwoole\Core\Utility\File;
 
 class ExcelUtility
 {
@@ -85,4 +87,67 @@ class ExcelUtility
         return $sheetData ?? [];
     }
 
+    /**
+     * [putCsv description]
+     * @param  string  $csvFileName [description] 文件名
+     * @param  array   $data     [description] 数组，每组数据都是使用，分割的字符串
+     * @param  string  $haderText   [description] 标题（默认第一行）
+     * @param  integer $line        [description] 从第几行开始写
+     * @param  integer $offset      [description] 共计写几行
+     * @return [type]               [description]
+     */
+    public static function putCsv(string $csvFileName, array $resultArray ,array $haderText = [], $line = 1, $offset = 0){
+
+        $objPHPExcel = new \PHPExcel();
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        if(!empty($haderText)){
+            // 合并标题
+            array_unshift($resultArray,$haderText);
+        }
+            
+        $indextoaz = array(
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        , 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ'
+        );
+
+        $tmpDir = Config::getInstance()->getConf('TEMP_DIR') . '/Exports';
+        $row = $line;
+        foreach ($resultArray as $data) {
+            $col = 0;
+            foreach ($data as $field) {
+                $objPHPExcel->getActiveSheet()->setCellValueExplicit($indextoaz[$col] . $row, $field, \PHPExcel_Cell_DataType::TYPE_STRING);
+                $objPHPExcel->getActiveSheet()->getStyle($indextoaz[$col] . $row)->getNumberFormat()->setFormatCode("@");
+                //            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($col, $row, $field);
+                $col++;
+            }
+            $row++;
+//            if($row % 1000 == 0){
+//                $objPHPExcel->createSheet();
+//                $objPHPExcel->setactivesheetindex($row / 1000);
+//            }
+        }
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $filePath = "{$tmpDir}/{$csvFileName}.xls";
+
+        File::createDir($tmpDir);
+        $objWriter->save($filePath);
+
+        // 保存至oss
+        $res = OssService::saveTmpFile($filePath,'tmp',$csvFileName.'_'.Time::getNowDataTime());
+        return $res ?? [];
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+

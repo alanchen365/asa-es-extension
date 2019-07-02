5d71eadbaf0fb8f0d1f75056da38988ebdb121c3
Bole <Achuner@AchunerdeMacBook-Pro.local>
2019-07-02 17:34:05 +0800

    更新rbc

--- src/Sdk/Baseservice/RbacService.php
+++ src/Sdk/Baseservice/RbacService.php
@@ -249,6 +249,28 @@ class RbacService extends BaseBaseservice {
         return $res ?? [];
     }
 
+
+    /**
+     * 重置密码
+     */
+    public static function resetPwd(int $userId, string $password, ?bool $isIgnoreErr = false) {
+        // 参数整理
+        $requestParams = [
+            'user_id' => $userId,
+            'password' => $password,
+            'system_id' => AppInfo::SYSTEM_ID,
+        ];
+
+        // 实例化请求类
+        $res = null;
+        $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
+        $remoteService->setIsIgnoreErr($isIgnoreErr);
+        $remoteService->getInstance(RpcConst::RBAC_RRC_SERVICE_CONF);
+        $res = $remoteService->request(RpcConst::RBAC_RRC_SERVICE_CONF['serviceName'],'User',__FUNCTION__,$requestParams);
+
+        return $res ?? [];
+    }
+
     #########################  用户相结束 #########################
 
 

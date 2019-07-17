0c132cc85804696ef48188e6e2e11d0779fc6b27
Bole <Achuner@AchunerdeMacBook-Pro.local>
2019-07-17 15:25:53 +0800

    更新rbac

--- src/Sdk/Baseservice/RbacService.php
+++ src/Sdk/Baseservice/RbacService.php
@@ -226,27 +226,6 @@ class RbacService extends BaseBaseservice {
         return $res;
     }
 
-    public static function addUserAll(array $accountList, string $name, string $password, array $roleIds = [],?bool $isIgnoreErr = false)
-    {
-        // 参数整理
-        $requestParams = [
-            'account_list' => $accountList,
-            'name' => $name,
-            'password' => $password,
-            'system_id' => AppInfo::SYSTEM_ID,
-            'role_ids' => $roleIds,
-        ];
-
-        // 实例化请求类
-        $res = null;
-        $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
-        $remoteService->setIsIgnoreErr($isIgnoreErr);
-        $remoteService->getInstance(RpcConst::RBAC_RRC_SERVICE_CONF);
-        $res = $remoteService->request(RpcConst::RBAC_RRC_SERVICE_CONF['serviceName'],'User',__FUNCTION__,$requestParams);
-
-        return $res;
-    }
-
     /**
      * 获取用户单条
      * @param int $userId

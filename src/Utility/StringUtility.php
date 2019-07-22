75dccf277263ef0a541594616b598b3dde9c1539
Bole <Achuner@AchunerdeMacBook-Pro.local>
2019-07-22 12:05:09 +0800

    更新字符串转换工具

--- src/Utility/StringUtility.php
+++ src/Utility/StringUtility.php
@@ -11,9 +11,9 @@ class StringUtility
     /*
      * 下划线转驼峰
      */
-    public static function convertUnderline($str)
+    public static function convertUnderline(string $str,string $separator = '_')
     {
-        $str = preg_replace_callback('/([-_]+([a-z]{1}))/i',function($matches){
+        $str = preg_replace_callback("/([-{$separator}]+([a-z]{1}))/i",function($matches){
             return strtoupper($matches[2]);
         },$str);
         return $str;
@@ -22,11 +22,10 @@ class StringUtility
     /*
      * 驼峰转下划线
      */
-    public static function humpToLine($str){
-        $str = preg_replace_callback('/([A-Z]{1})/',function($matches){
-            return '_'.strtolower($matches[0]);
+    public static function humpToLine(string $str,string $separator = '_'){
+        $str = preg_replace_callback('/([A-Z]{1})/',function($matches) use($separator){
+            return $separator.strtolower($matches[0]);
         },$str);
-        return ltrim($str, "_");
+        return ltrim($str, $separator);
     }
-
 }

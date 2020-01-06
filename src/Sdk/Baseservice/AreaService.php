93138540a4829171a6c300b2064a33e2acccb8c5
alanchen365 <alanchen365@gmail.com>
2020-01-06 09:44:01 +0800

    加入地区sdk

--- src/Sdk/Baseservice/AreaService.php
+++ src/Sdk/Baseservice/AreaService.php
@@ -42,4 +42,29 @@ class AreaService extends BaseBaseservice {
 
         return $res['area_names'] ?? [];
     }
+
+    /**
+     * 根据地区名称获取地区信息
+     * @param int $userId
+     * @param bool $isIgnoreErr
+     * @return array
+     */
+    public static function getAreaByAreaName(string $areaName, int $parentId = 0, bool $isIgnoreErr = false): array
+    {
+        // 参数整理
+        $requestParams = [
+            'area_name' => $areaName,
+            'parent_id' => $parentId,
+            'system_id' => AppInfo::SYSTEM_ID,
+        ];
+
+        // 实例化请求类
+        $res = null;
+        $remoteService = new RemoteService(RemoteService::REQUEST_WAY_RPC);
+        $remoteService->setIsIgnoreErr($isIgnoreErr);
+        $remoteService->getInstance(RpcConst::AREA_RRC_SERVICE_CONF);
+        $res = $remoteService->request(RpcConst::AREA_RRC_SERVICE_CONF['serviceName'], 'Area', __FUNCTION__, $requestParams);
+
+        return $res['area_list'] ?? [];
+    }
 }

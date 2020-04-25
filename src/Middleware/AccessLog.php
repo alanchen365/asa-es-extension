11a9cd32b249a8de8cfdfcd0e400fe96cb73d4bf
alanchen365 <alanchen365@gmail.com>
2020-04-25 14:18:16 +0800

    调整慢日志时间 为500毫秒

--- src/Middleware/AccessLog.php
+++ src/Middleware/AccessLog.php
@@ -33,16 +33,17 @@ class AccessLog
             //获取用户IP地址
             $ip = ServerManager::getInstance()->getServer()->connection_info($request->getSwooleRequest()->fd);
             $requestObj = Di::getInstance()->get(AsaEsConst::DI_REQUEST_OBJ);
-            
+
             //拼接一个简单的日志
-            $logStr = $ip['remote_ip'] .' | '. ($runTime * 1000) . ' ms | ' . $request->getUri() .' | '. $requestObj->getRequestId() . ' | ' .  $request->getHeader('user-agent')[0];
+            $runTime = round(floatval($runTime * 1000), 2);
+            $logStr = $ip['remote_ip'] . ' | ' . $runTime . ' ms | ' . $request->getUri() . ' | ' . $requestObj->getRequestId() . ' | ' . $request->getHeader('user-agent')[0];
+
             //判断一下当执行时间大于1秒记录到 slowlog 文件中，否则记录到 access 文件
-            if (($runTime * 1000) > 300) {
+            if ($runTime < round(600, 2)) {
                 FileLogger::getInstance()->log($logStr, AsaEsConst::LOG_ACCESS);
             } else {
                 FileLogger::getInstance()->log($logStr, AsaEsConst::LOG_SLOW);
             }
-            
         } catch (\Throwable $throwable) {
             throw new MiddlewareException($throwable->getCode(), $throwable->getMessage());
         }
